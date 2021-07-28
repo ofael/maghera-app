10cc0cbf65ea533b525c24e63686a70ff8721b6b
ofael <fael_assis@hotmail.com>
2021-07-28 12:40:14 -0300

    Creating layout create account and layout main task

--- app/src/main/java/app/application/raghemaapp/MainActivity.kt
+++ app/src/main/java/app/application/raghemaapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package app.application.raghemaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCriarConta = findViewById<Button?>(R.id.btn_criar_conta)
+
+        btnCriarConta.setOnClickListener {
+            val intent = Intent(this,ActivityCriarConta::class.java)
+            startActivity(intent)
+        }
     }
 }
